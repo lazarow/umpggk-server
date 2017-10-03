637d3abb05ba8395ba99042ee91509733b906db3
Arek Nowakowski <a.nowakowski@devesoft.pl>
2017-10-03 11:30:55 +0200

    Add socket.io client

--- server/lib/webapp-server.js
+++ server/lib/webapp-server.js
@@ -12,6 +12,9 @@ WebAppServer.prototype.start = function (options) {
     this.server.listen(options.port);
     io.on('connection', function (socket) {
         console.log('A new websocket connection...');
+        socket.on('disconnect', function () {
+            console.log('The websocket disconnection...');
+        });
     });
     console.log("The web app server is listening on " + this.server.server.address().address + ":" + this.server.server.address().port);
 };
